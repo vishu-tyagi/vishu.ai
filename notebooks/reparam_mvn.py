c622d9be499dd6e7828aca95820f4b3d886bebd4
Vishu Tyagi <vishweshwartyagiv@gmail.com>
2026-01-18 23:13:49 -0500

    z

--- notebooks/reparam_mvn.py
+++ notebooks/reparam_mvn.py
@@ -1,26 +1,32 @@
 import jax.numpy as jnp
 from jax import random
-from numpyro.distributions import MultivariateNormal
+from numpyro import distributions as dist
 
 # Setup
 key = random.PRNGKey(0)
-num_samples = 1000
-dim = 3
+num_samples = 1000  # N = 1_000
+dim = 3             # d = 3
 
-# Generate mean and covariance
+# Generate mean
+key, sub = random.split(key)
+mu = random.normal(sub, (dim,))
+
+# Generate covariance
+key, sub = random.split(key)
+A = random.normal(sub, (dim, dim))
+cov = A @ A.T   # ensure positive definite
+
+# Generate samples directly
+key, sub = random.split(key)
+direct = dist.MultivariateNormal(mu, cov).sample(sub, (num_samples,))
+
+# Lower Cholesky factor 
+L = jnp.linalg.cholesky(cov)    # (d, d)
+
+# Generate samples with reparametrization
 key, subkey = random.split(key)
-mu = random.normal(subkey, (dim,))
-A = random.normal(subkey, (dim, dim))
-cov = A @ A.T  # ensure positive-definite
-
-# Sample directly
-samples = MultivariateNormal(mu, cov).sample(subkey, (num_samples,))
-
-L = jnp.linalg.cholesky(cov)
-
-key, subkey = random.split(key)
-z = random.normal(subkey, (num_samples, dim))
-samples_reparam = mu + z @ L.T
+z = random.normal(subkey, (num_samples, dim))   # (N, d)
+reparam = mu + jnp.einsum("ij,nj->ni", L, z)
 
 import numpy as np
 import pandas as pd
@@ -28,7 +34,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from util import custom_pairplot
 
-fig, axes = custom_pairplot(samples, samples_reparam)
+fig, axes = custom_pairplot(direct, reparam)
 
 import os
 from constants import BUILD_DIR, BG_COLOR
@@ -42,4 +48,5 @@ for ax in fig.axes:
     ax.set_facecolor(site_bg)
 output_path = os.path.join(BUILD_DIR, "reparam_mvn.png")
 fig.savefig(output_path, dpi=300)
+print(f"Saved to {output_path}")
 plt.close(fig)
